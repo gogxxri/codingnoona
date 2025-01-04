e6b96507d13c9ca1dc0ea9aeaa019b680a1e3a58
gyuri <rbfl7645@naver.com>
2025-01-04 15:21:38 +0900

    #1. 리액트 기초 : 컴포넌트, props

---
+++ first-react/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Box from  "./components/Box"
+
+function App() {
+  return (
+      <div>
+          <Box name="규리" num="1"/>
+          <Box name="정훈" num="2"/>
+          <Box name="수혁" num="3"/>
+      </div>
+  );
+}
+
+export default App;
