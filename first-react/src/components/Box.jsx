999706b6e1d61d27cbca14506d4e454b758fb851
gyuri <rbfl7645@naver.com>
2025-01-05 14:07:00 +0900

    #1. 리액트 기초 : 가위바위보! (강의)

--- first-react/src/components/Box.jsx
+++ first-react/src/components/Box.jsx
@@ -6,13 +6,13 @@ const Box = ({ title, item, result }) => {
     comResult = 'tie';
   } else if (result === 'win') {
     comResult = 'lose';
-  } else {
+  } else if (result === 'lose') {
     comResult = 'win';
   }
   return (
     <div className={`box ${title === 'You' ? result : comResult}`}>
       <h1>{title}</h1>
-      <img src={item && item.img} alt="rock" />
+      {item ? <img src={item && item.img} alt="rock" /> : <p>버튼을 눌러보세요</p>}
       {title === 'You' ? <p>{result}</p> : <p>{comResult}</p>}
     </div>
   );
